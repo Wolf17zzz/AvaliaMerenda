fab903f8809fc636a7fc3a695b2e73f9d91c2a27
Guilherme Mestriner <gm5842039@gmail.com>
2023-03-29 22:36:29 -0300

    cardapio1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,6 +9,9 @@
                         
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registro</a>
+                        
+                        @if (Route::has('cardapio'))
+                            <a href="{{ route('cardapio') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Cardapio</a>
                         @endif
                     @endauth
                 </div>
